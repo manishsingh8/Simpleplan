529e2515ee35f76c7197744c5742b02f1adb9377
Manish Singh <manishsingh04031998@gmail.com>
2023-07-28 22:11:56 +0530

    Body Page

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React from 'react';
+import './body.css';
+import img from '../images/image1.png';
 
 const Body = () => {
   return (
     <div className='body-container'>
      <div className='body-inner'>
-
+         <div className='body-heading'>
+           <h3>Hobbies</h3>
+           <h3>Happiness</h3>
+           <h3>Home</h3>
+         </div>
+         <div className='body-para'>
+          A Nordic Startup which brings incredibly<br/> intresting hobbies
+          from around the world to<br/> people of all ages.
+         </div>
+         <div className='body-button'>
+          <input type="text" placeholder='Enter your email' className='email' />
+          <button>START EXPLORING</button>
+         </div>
      </div>
-      
+     <div className="image-section">
+         <img src={img} alt="image..." className='body-image' />
+     </div>
     </div>
   )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Header from './Header';
+import Body from './Body';
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Body />
+      <button style={{borderRadius:"50%",position:"absolute",top:"80%",bottom:"10%",left:"90%"}}>floating</button>
     </div>
   )
 }
