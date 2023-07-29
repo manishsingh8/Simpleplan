2af5ecf6c7c02dfcfdca5d7551082b2f367b5506
Manish Singh <manishsingh04031998@gmail.com>
2023-07-29 18:06:31 +0530

    Plan-Page

---
+++ src/components/Body/Plan/Plan.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Plancomp from './Plancomp';
+import './plan.scss';
+const Plan = () => {
+  return (
+    <div className='plan-container'>
+        <span>
+        <h3>Discover Classes  <a href="">SHOW ALL</a></h3>
+        <div>
+            <p>show prices in: <button>click</button></p>
+        </div>
+        </span>
+        
+      <p>Choose from veraity of classes from over the world</p>
+      <div style={{display:"flex",justifyContent:"space-between"}}>
+        
+        <Plancomp />
+        <Plancomp />
+        <Plancomp />
+        <Plancomp />
+      </div>
+    </div>
+  )
+}
+
+export default Plan
