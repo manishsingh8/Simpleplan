879ce2836807241fa406e07288aaf354dae2aa41
Manish Singh <manishsingh04031998@gmail.com>
2023-07-30 01:44:26 +0530

    Teacher-Section

---
+++ src/components/Body/Teacher/Teacher.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Carousel from './Carousel';
+
+const Teacher = () => {
+  return (
+    <div>
+        <h1>Meet our teachers</h1>
+      <Carousel />
+    </div>
+  )
+}
+
+export default Teacher;
