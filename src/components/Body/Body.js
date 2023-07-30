89a410e6bfc3c53c967abd39d3299b153c00fd4b
Manish Singh <manishsingh04031998@gmail.com>
2023-07-30 15:05:11 +0530

    Body-Responsive

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,28 +1,35 @@
-import React from 'react';
-import './body.css';
-import img from '../../images/image1.png';
+import React from "react";
+import "./body.scss";
+import img from "../../images/image1.png";
 
 const Body = () => {
   return (
-    <div className='body-container'>
-     <div className='body-inner'>
-         <div className='body-heading'>
-           <h3>Hobbies ❤️ Happiness ❤️ Home</h3>
-         </div>
-         <div className='body-para'>
-          A Nordic Startup which brings incredibly<br/> intresting hobbies
-          from around the world to<br/> people of all ages.
-         </div>
-         <div className='body-button'>
-          <input type="text" placeholder='Enter your email' className='email' />
-          <button>START EXPLORING</button>
-         </div>
-     </div>
-     <div className="image-section">
-         <img src={img} alt="image..." className='body-image' />
-     </div>
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-6 colm-1">
+          <h3>Hobbies ❤️ Happiness ❤️ Home</h3>
+          <p>
+            A Nordic Startup which brings incredibly <br /> intresting hobbies
+            from around the world to <br />
+            people of all ages.
+          </p>
+          <div className="body-btn">
+            <input
+              type="text"
+              placeholder="Enter your email"
+              className="email"
+            />
+            <button className="body-btn">START EXPLORING <i class="fa-solid fa-circle-arrow-right fa-fade" /></button>
+          </div>
+        </div>
+        <div className="col-sm-6 colm-2">
+          <div className="body-img">
+            <img src={img} alt="image..." className="body-image" />
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
