9e0f89761929ab7de54b1e79c49cc458a442a067
Manish Singh <manishsingh04031998@gmail.com>
2023-08-01 20:18:54 +0530

    Plan-Component

--- src/components/Body/Plan/Plan.js
+++ src/components/Body/Plan/Plan.js
@@ -1,22 +1,28 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Plancomp from './Plancomp';
 import './plan.scss';
 const Plan = () => {
+
+  const[price,setPrice] = useState('₹1999 only'); 
+  
   return (
     <div className='plan-container'>
         <span>
         <h3>Discover Classes  <a href="">SHOW ALL</a></h3>
-        <div>
-            <p>show prices in: <button>click</button></p>
+        <div style={{display:"flex",alignItems:"center"}}>
+         <p style={{marginTop:"11px",marginRight:"10px"}}> show prices in : </p>  
+        <button className="btn1" onClick={()=>{setPrice('₹1999 only')}}>INR</button>
+        <button className="btn2" onClick={()=>{setPrice('$25 only')}}>USA</button>
         </div>
         </span>
         
       <p>Choose from veraity of classes from over the world</p>
       <div className='plan-comp'>
-        <Plancomp />
-        <Plancomp />
-        <Plancomp />
-        <Plancomp />
+        {/* PlanComponent */}
+        <Plancomp price={price}/>
+        <Plancomp price={price}/>
+        <Plancomp price={price}/>
+        <Plancomp price={price}/>
       </div>
     </div>
   )
--- src/components/Body/Plan/Plancomp.js
+++ src/components/Body/Plan/Plancomp.js
@@ -1,22 +1,28 @@
-import React from 'react';
-import img from '../../../images/book2.jpg';
-import './plancomp.scss';
+import React from "react";
+import img from "../../../images/book2.jpg";
+import "./plancomp.scss";
 
-const Plancomp = () => {
+const Plancomp = (props) => {
   return (
-    
-       <div className="plan-card">
-        {/* <div className='plan-inner'> */}
-            <img src={img} className="card-img-top" alt="..." style={{borderTopLeftRadius:"10px",borderTopRightRadius:"10px"}} />
-            <p style={{color:"#2430A0 ",paddingLeft:"4px"}}>LANGUAGE</p>
-            <h6 className="card-title">Class name- Lorem ipsum dolor sit amet, consectetur adipisi.</h6>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
-
-        </div>
-        //  </div>
-        
-  )
-}
+    <div className="plan-card">
+      <img
+        src={img}
+        className="card-img-top"
+        alt="..."
+        style={{ borderTopLeftRadius: "10px", borderTopRightRadius: "10px" }}
+      />
+      <p style={{ color: "#2430A0 ", paddingLeft: "4px" ,fontWeight:"700"}}>LANGUAGE</p>
+      <h6 className="card-title">
+        Class name- Lorem ipsum dolor sit amet, consectetur adipisi.
+      </h6>
+      <p className="card-text">
+      Lorem ipsum dolor sit amet, varius consectetur varius entat consectet adipiscing elit. Ullamcorper...
+      </p>
+      <a href="#" className="btn btn-primary">
+     <span style={{display:"flex",justifyContent:"space-between"}}><h6>{props.price}</h6> <h6>Enroll now</h6> </span> 
+      </a>
+    </div>
+  );
+};
 
 export default Plancomp;
