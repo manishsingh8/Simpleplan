931941611c65cb511f08db21d87cb42a19fe79a6
Manish Singh <manishsingh04031998@gmail.com>
2023-08-01 20:15:10 +0530

    Window-Scroll

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -3,11 +3,12 @@ import "./body.scss";
 import img from "../../images/image1.png";
 
 const Body = () => {
+  const heartIcon = <i className="fa-solid fa-heart fa-beat" style={{color:"#fa1919"}}/>
   return (
     <div className="container">
       <div className="row">
-        <div className="col-sm-6 colm-1">
-          <h3>Hobbies ❤️ Happiness ❤️ Home</h3>
+        <div className="col-md-6 colm-1">
+          <h3>Hobbies {heartIcon} Happiness {heartIcon} Home</h3>
           <p>
             A Nordic Startup which brings incredibly <br /> intresting hobbies
             from around the world to <br />
@@ -22,7 +23,7 @@ const Body = () => {
             <button className="body-btn">START EXPLORING <i class="fa-solid fa-circle-arrow-right fa-fade" /></button>
           </div>
         </div>
-        <div className="col-sm-6 colm-2">
+        <div className="col-md-6 colm-2">
           <div className="body-img">
             <img src={img} alt="image..." className="body-image" />
           </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,49 @@
-import React from 'react';
-import Header from './Header/Header';
-import Body from './Body/Body';
-import Footer from './Footer/Footer';
-import Category from './Body/Category/Category';
-import Blog from './Body/Blog/Blog';
-import Class from './Body/Class/Class';
-import Plan from './Body/Plan/Plan';
-import Explore from './Body/Explore/Explore';
-import Teacher from './Body/Teacher/Teacher';
+import React from "react";
+import Header from "./Header/Header";
+import Body from "./Body/Body";
+import Footer from "./Footer/Footer";
+import Category from "./Body/Category/Category";
+import Blog from "./Body/Blog/Blog";
+import Class from "./Body/Class/Class";
+import Plan from "./Body/Plan/Plan";
+import Explore from "./Body/Explore/Explore";
+import Teacher from "./Body/Teacher/Teacher";
+import Tooltip from "@mui/material/Tooltip";
 
 const Home = () => {
+  // Function to handle smooth scroll to top
+  const handleScrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", // Add smooth behavior
+    });
+  };
+
   return (
-    <div style={{marginLeft:"10%",marginRight:"10%"}}>
+    <div style={{ marginLeft: "10%", marginRight: "10%"}}>
       <Header />
       <Body />
       <Explore />
-      <button style={{borderRadius:"50%",position:"absolute",top:"80vh",bottom:"10vh",left:"90vw"}}>floating</button>
       <Plan />
       <Category />
       <Class />
       <Teacher />
       <Blog />
+
+      {/* Go to top button */}
+      <Tooltip title="Go To Top" placement="bottom-start">
+        <button onClick={handleScrollToTop} style={{ float: "right", margin: "10px", border: "none", background: "transparent", cursor: "pointer" }}>
+          <i
+            className="fa-solid fa-circle-chevron-right fa-rotate-270 fa-2xl"
+            style={{
+              color: "#2430A0",
+            }}
+          />
+        </button>
+      </Tooltip>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
