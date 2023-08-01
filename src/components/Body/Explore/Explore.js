06a86d4b3ae8060d4dbfde8f295111c73ec71fdd
Manish Singh <manishsingh04031998@gmail.com>
2023-08-01 20:19:39 +0530

    Explore-Section-Update

--- src/components/Body/Explore/Explore.js
+++ src/components/Body/Explore/Explore.js
@@ -6,6 +6,29 @@ import img3 from "../../../images/learn.webp";
 import img4 from "../../../images/teacher4.webp";
 
 const Explore = () => {
+  const images = [
+    {
+      img: img1,
+      heading: "Pay as you go",
+      text: "Explore your passion and go beyond the physical and mental boundaries of time age gender or geography",
+    },
+    {
+      img: img2,
+      heading: "Take classes anytime, anywhere",
+      text: "We offers Interactive classes by experts who are qualified and trusted.",
+    },
+    {
+      img: img3,
+      heading: "Learn something new",
+      text: " Join sessions at your own convenience and pace, from the comforts of your home.",
+    },
+    {
+      img: img4,
+      heading: "Skilled & Passionate Teachers",
+      text: "No enrollment fee for our classes. You only pay for the classes that you take. Your payment is Safe and secure with us.",
+    },
+  ];
+
   return (
     <div className="explore-container">
       <div className="explore-page">
@@ -16,46 +39,17 @@ const Explore = () => {
         </h3>
       </div>
       <div className="explore-card">
-        <div className="exp-img">
-          <div>
-            <img src={img1} alt="" />
-          </div>
-          <h6>Pay as you go</h6>
-          <p>
-            Explore your passion and go beyond the physical and mental
-            boundaries of time, age, gender or geography.
-          </p>
-        </div>
-        <div className="exp-img">
-          <div>
-            <img src={img2} alt="" />
-          </div>
-          <h6>Take classes anytime, anywhere</h6>
-          <p>
-            We offers Interactive classes by experts who are qualified and
-            trusted.
-          </p>
-        </div>
-        <div className="exp-img">
-          <div>
-            <img src={img3} alt="" />
-          </div>
-          <h6>Learn something new</h6>
-          <p>
-            Join sessions at your own convenience and pace, from the comforts of
-            your home.
-          </p>
-        </div>
-        <div className="exp-img">
-          <div>
-            <img src={img4} alt="" />
-          </div>
-          <h6>Skilled & Passionate Teachers</h6>
-          <p>
-            No enrollment fee for our classes. You only pay for the classes that
-            you take. Your payment is Safe and secure with us.
-          </p>
-        </div>
+        {images.map((value, index) => {
+          return (
+            <div className="exp-img" key={index}>
+              <div>
+                <img src={value.img} alt="" />
+              </div>
+              <h6>{value.heading}</h6>
+              <p> {value.text}</p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
