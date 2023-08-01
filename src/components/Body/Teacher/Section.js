03a0d7f1d5d7fccdd9ca7c2713fda8fd4388e733
Manish Singh <manishsingh04031998@gmail.com>
2023-08-01 20:17:25 +0530

    Section-Icon-added

--- src/components/Body/Teacher/Section.js
+++ src/components/Body/Teacher/Section.js
@@ -6,20 +6,20 @@ const Section = () => {
   return (
     <div className="container">
     <div className="row">
-        <div className="col-sm-6 column1">
+        <div className=" col-md-6 column1">
             <div className='sec-img'>
             <img src={teacher} alt=""  />
             </div>
         </div>
-        <div className="col-sm-6 column2">
-           <h3>Become a certified teacher</h3>
+        <div className=" col-md-6 column2">
+           <h3 style={{color:"#2430A0",fontFamily:"font-family: 'Poppins', sans-serif;",fontWeight:"700"}}>Become a certified teacher</h3>
            <p>we only have the best and trusted teachers from the glob. Just join us if you have skill and passion to share </p>
-           <ul type="square">
-            <li>Make you own schedule</li>
-            <li>Teach students on an international platform</li>
-            <li>Become part of international community of passionate educators</li>
+           <ul>
+            <li><i class="fa-solid fa-stop fa-beat" /> Make you own schedule</li>
+            <li><i class="fa-solid fa-stop fa-beat"/> Teach students on an international platform</li>
+            <li><i class="fa-solid fa-stop fa-beat"/> Become part of international community of passionate educators</li>
            </ul>
-           <button>START TEACHING</button>
+           <button>START TEACHING  <i class="fa-solid fa-circle-arrow-right fa-fade"></i></button>
         </div>
     </div>
     </div>
