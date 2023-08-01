4d729ebd8bd5a9229635d9c414cd15e1b92de559
Manish Singh <manishsingh04031998@gmail.com>
2023-08-01 20:16:26 +0530

    Carousel-section

--- src/components/Body/Teacher/Carousel.js
+++ src/components/Body/Teacher/Carousel.js
@@ -1,42 +1,51 @@
 import React, { useRef, useState } from "react";
 import "./carousel.scss";
 import T1 from "../../../images/T1.jpg";
-import T2 from "../../../images/T2.jpg";
-import T3 from "../../../images/T7.jpg";
+import T2 from "../../../images/w2.jpg";
+import T3 from "../../../images/T3.webp";
 import T4 from "../../../images/T4.avif";
 import T5 from "../../../images/T5.png";
 import T6 from "../../../images/T7.jpg";
+import Tooltip from "@mui/material/Tooltip";
 
 const Slider = () => {
+  // Teachers list with their images and specialties
   const images = [
     { image: T1, name: "Kally", speciality: "YOGA" },
-    { image: T2, name: "Jhon", speciality: "ARTS & CRAFT" },
-    { image: T3, name: "Lili", speciality: "ACADEMICS" },
+    { image: T2, name: "Kethrin", speciality: "ARTS & CRAFT" },
+    { image: T3, name: "Smith", speciality: "ACADEMICS" },
     { image: T4, name: "Seyma", speciality: "SPORTS" },
     { image: T5, name: "Rechal", speciality: "MUSIC" },
     { image: T6, name: "Porter", speciality: "DANCING" },
-    { image: T2, name: "Cheiml", speciality: "COMPUTER" },
-    // Add more image URLs here
   ];
 
+  // Ref to the carousel container
   const sliderRef = useRef(null);
-  const slideWidth = 300; // Width of each slide (adjust as needed)
-  const slidesPerPage = 4; // Number of slides visible at a time
 
+  // Number of slides to show per page
+  const slidesPerPage = 4;
+
+  // State to keep track of the current index in the slider
   const [currentIndex, setCurrentIndex] = useState(0);
 
+  // Function to handle previous button click
   const handlePrev = () => {
     if (currentIndex === 0) {
+      // If at the first set of slides, loop to the last set
       setCurrentIndex(images.length - slidesPerPage);
     } else {
+      // Move to the previous set of slides
       setCurrentIndex((prevIndex) => prevIndex - 1);
     }
   };
 
+  // Function to handle next button click
   const handleNext = () => {
     if (currentIndex === images.length - slidesPerPage) {
+      // If at the last set of slides, loop to the first set
       setCurrentIndex(0);
     } else {
+      // Move to the next set of slides
       setCurrentIndex((prevIndex) => prevIndex + 1);
     }
   };
@@ -44,25 +53,43 @@ const Slider = () => {
   return (
     <div className="slider-container">
       <div className="carousel-container" ref={sliderRef}>
-        {images.slice(currentIndex, currentIndex + slidesPerPage)
+        {/* Render the visible slides */}
+        {images
+          .slice(currentIndex, currentIndex + slidesPerPage)
           .map((img, index) => (
             <div className="caro-img-card" key={index}>
+              {/* Display the teacher's image */}
               <img
                 src={img.image}
                 className="card-img-top carousel-cards"
                 alt="..."
               />
-              <div>
+              <div className="speciality">
+                {/* Display the teacher's name */}
                 <h6>{img.name}</h6>
-               <p>{img.speciality}</p>
+                {/* Display the teacher's specialty */}
+                <p>{img.speciality}</p>
               </div>
-              
             </div>
           ))}
       </div>
-        <div className="slider-controls">
-        <button className="pre" onClick={handlePrev}><i className="fa-solid fa-circle-chevron-left fa-beat fa-2xl"></i></button>
-        <button className="next" onClick={handleNext}><i className="fa-solid fa-circle-chevron-right fa-beat fa-2xl"></i></button>
+      {/* Slider navigation controls */}
+      <div className="slider-controls">
+        <Tooltip title="Back" placement="left-start">
+          <i
+            className="fa-solid fa-circle-chevron-left fa-beat fa-2xl pre"
+            onClick={handlePrev}
+            style={{ cursor: "pointer" }}
+          />
+        </Tooltip>
+
+        <Tooltip title="Next" placement="right-start">
+          <i
+            className="fa-solid fa-circle-chevron-right fa-beat fa-2xl next"
+            onClick={handleNext}
+            style={{ cursor: "pointer" }}
+          />
+        </Tooltip>
       </div>
     </div>
   );
