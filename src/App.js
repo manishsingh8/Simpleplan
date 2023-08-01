1f9d35d7521f022de0bbe0a1cd0c87b170e5de14
Manish Singh <manishsingh04031998@gmail.com>
2023-08-01 20:13:44 +0530

    Modal

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import './App.css';
-import Home from './components/Home';
-
-
+import React from "react";
+import "./App.css";
+import Home from "./components/Home";
+import Tooltip from "@mui/material/Tooltip";
+import ModalComp from "./Modal/Modal";
 
 function App() {
   return (
     <div className="App">
-    <Home />
+      <Home />
+
+      {/* floating chat button */}
+
+      <ModalComp />
+     
     </div>
   );
 }
